291f544691963b9d834a3d621626253e6939e6eb
matesa <75279583+matesa@users.noreply.github.com>
2020-12-30 18:40:47 +0300

    Create gecici_alan_temizleyici.py

---
+++ Robot/Edevat/gecici_alan_temizleyici.py
@@ -0,0 +1,9 @@
+# Bu araç @matesa tarafından | @POLESTAR_CHAT için yazılmıştır.
+
+from os import remove
+from glob import glob
+
+async def icinden_gec(dizin) -> None:
+    gecici_liste = glob(f"{dizin}/*.*")
+    for dosya in gecici_liste:
+        remove(dosya) 
